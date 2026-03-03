760d33051b6f216ce4c3f0eaf42e24f2de1f2990
nanke <1154685452@qq.com>
2026-03-03 23:23:43 +0800

    多线程循环打印

--- src/main/java/com/nk/lc/thread/ABC.java
+++ src/main/java/com/nk/lc/thread/ABC.java
@@ -4,54 +4,69 @@ import java.util.Objects;
 
 public class ABC {
 
-    private static String state = "A";
+    private static int state = 1;
+
+    private static int count = 3;
+
     private static Object lock = new Object();
 
     public static void main(String[] args) {
 
-        Thread t1 = new Thread(new Say("A"));
-        Thread t2 = new Thread(new Say("B"));
-        Thread t3 = new Thread(new Say("C"));
+        Thread t1 = new Thread(new Say(1));
+        Thread t2 = new Thread(new Say(2));
+        Thread t3 = new Thread(new Say(3));
 
-        t3.start();
-        t2.start();
         t1.start();
+        t2.start();
+        t3.start();
 
     }
 
     private static class Say implements Runnable {
 
-        private String target;
+        int num;
 
-        public Say(String target) {
-            this.target = target;
+        int total = 0;
+
+        public Say(int num) {
+            this.num = num;
         }
 
-        @Override
         public void run() {
-
             while (true) {
                 synchronized (lock) {
-                    while (!Objects.equals(target,state)) {
+
+                    if (total > count) {
+                        break;
+                    }
+
+                    while (state != num) {
                         try {
                             lock.wait();
                         } catch (InterruptedException e) {
-                            throw new RuntimeException(e);
+                            e.printStackTrace();
+                            return;
                         }
                     }
 
-                    System.out.println(target);
-                    if (Objects.equals(state,"A")) {
-                        state = "B";
-                    } else if (Objects.equals(state,"B")) {
-                        state = "C";
-                    } else if (Objects.equals(state,"C")) {
-                        state = "A";
+                    System.out.println(num);
+                    if (state == 1) {
+                        state = 2;
+                    } else if (state == 2) {
+                        state = 3;
+                    } else {
+                        state = 1;
                     }
+                    total ++;
+
                     lock.notifyAll();
                 }
+
+
             }
+
         }
+
     }
 
 
